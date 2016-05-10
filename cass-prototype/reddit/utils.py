487cf1a76bca150770d6579ddf7c4315d3af18ae
Will Liu <will.liu@tivix.com>
2016-05-10 16:04:35 -0400

    Add util to generate some uuids for testing

--- cass-prototype/reddit/utils.py
+++ cass-prototype/reddit/utils.py
@@ -1,18 +1,30 @@
 import json
 import csv
 import os
+import uuid
 
 from django.conf import settings
 from django.db import connection
 
 
-if __name__ == '__main__':
-
+def load_json():
     my_file = open('/Users/williamliu/Desktop/data/smallSampleTweets.json')
     data = json.load(my_file)
     print data
 
+
+def execute_raw_cql():
     # settings.configure()
     cursor = connection.cursor()
     result = cursor.execute("SELECT COUNT(*) FROM twitterbot")
     print result[0]['count']
+
+
+def generate_uuid(times=10):
+    """ Give some random UUIDs for testing """
+    for item in xrange(times):
+        print uuid.uuid4()
+
+
+if __name__ == '__main__':
+    generate_uuid()
